89178cb727bad5e55c463e7a72838303d66ec574
Khalil <khalilpeguero@hotmail.com>
2023-09-16 16:19:03 -0400

    Grammar

--- script.js
+++ script.js
@@ -5,36 +5,36 @@ const diagnosisTree = {
         yes: {
             question: " Does your throat burn when you drink water? ",
             yes: {
-                result: "You may have Rabies"
+                result: "You may have Rabies."
             },
             no: {
-                result: "Seek medical help"
+                result: "Seek medical help."
             }
         },
         no: {
             question: " Have you experienced one or more swollen, painful lymph nodes? ",
             yes: {
-                result: "You may have the Plague"
+                result: "You may have the Plague."
             },
             no: {
                 question: " Have you experienced any gastrointestinal issues? ",
                 yes: {
                     question: " Any unexplanied bleeding or bruising? ",
                     yes: {
-                        result: "You may have Ebola"
+                        result: "You may have Ebola."
                     },
                     no: {
-                        result: "Seek medical help"
+                        result: "Seek medical help."
                     }
                 },
                 no: {
-                    result: "Seek medical help"
+                    result: "Seek medical help."
                 }
             }
         }
     },
     no: {
-        result: "Seek medical help"
+        result: "Seek medical help."
     }
 };
     let currentNode = diagnosisTree;
