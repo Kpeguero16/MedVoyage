fdc96dc8b2dba88977a1e2fa5aa6d223d2468e98
chrisv0912 <107316682+chrisv0912@users.noreply.github.com>
2023-09-16 13:17:49 -0400

    Add files via upload

---
+++ Ebola.java
@@ -0,0 +1,28 @@
+package diagnosis;
+import java.util.Scanner;
+
+public class Ebola extends Disease{
+    private static final String[] ebolaSymptoms = {"Flu-like symptoms", "Loss of appetite", "Gastrointestinal symptoms", "Unexplained hemorrhaging, bleeding, or bruising"};
+    public Ebola(String name, String[] symptoms) {
+        super(name, symptoms);
+
+    }
+    public static boolean checkForEbola(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Do you have " + ebolaSymptoms[2] + " ?");
+        String hasGastroSymp = input.nextLine();
+        if (hasGastroSymp.equals("yes")){
+            System.out.println("Have you had diarrhea?");
+            String diarrhea = input.nextLine();
+            if (diarrhea.equals("yes")){
+                System.out.println("Have you had unexplained bleeding?");
+                String bleeding = input.nextLine();
+                if (bleeding.equals("yes")){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
