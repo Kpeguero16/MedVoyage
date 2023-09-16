382926ab444ec96ade0b3aa58aeda22feebc88d5
Mattjava <77417435+Mattjava@users.noreply.github.com>
2023-09-16 14:57:52 -0400

    Added some comments to the Disease class

--- diagnosis/src/Disease.java
+++ diagnosis/src/Disease.java
@@ -1,5 +1,11 @@
 package diagnosis.diagnosis.src;
 
+/*
+    Parent class of all the diseases listed.
+    Each disease will have a symptom and a name.
+    Name - a string containing the medical term for the disease or condition
+    Symptoms - A string array that contains the symptoms of the disease.
+ */
 public class Disease {
     private String[] symptoms;
     private String name;
